d5fbc4af1a6901b485e4c1feac681d9bbf363707
Ayush <ayush.tesla@gmail.com>
2025-05-30 21:17:24 +0530

    CORS Errors fixed

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 connectDB();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://ayushtaskmanagerapp.netlify.app",
+  credentials: true, // only if using cookies/JWT in auth headers
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
